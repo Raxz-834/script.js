7a3323eb04a7279d26e680a28b69d0e4ad407142
Raxz-834 <rrahadiawan@gmail.com>
2024-10-16 08:10:15 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function showMessage() {
 
     // Ubah link WhatsApp saat pesan ditampilkan
     const whatsappButton = document.getElementById('whatsappButton');
-    const encodedMessage = encodeURIComponent("Aku sayang kamu, dan aku akan selalu mencintaimu selamanya ❤️");
-    const phoneNumber = '628123456789'; // Ganti dengan nomor WhatsApp tujuan
+    const encodedMessage = encodeURIComponent("Aku sayang kamu, dan aku akan selalu mencintaimu selamanya,oh iya sama jangan ngeselin yaa sayang,jangan ngomong kasar, love you sayang Firanti Anjani nya akuuu ❤️");
+    const phoneNumber = '62882000611369'; // Ganti dengan nomor WhatsApp tujuan
     whatsappButton.href = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
 }
